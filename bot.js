c6689934990c241928d13ae65fe28e376362079b
David190504 <desr11@hotmail.com>
2019-10-08 23:05:18 -0700

    answer and now

--- bot.js
+++ bot.js
@@ -30,8 +30,12 @@ function processCommand(msg) {
         helpCommand(arguments, msg)
     } else if (primaryCommand == "multiply") {
         multiplyCommand(arguments, msg)
-    } else if(primaryCommand === "say") {
+    } else if (primaryCommand == "say") {
         sayCommand(arguments, msg)
+    } else if (primaryCommand == "answer") {
+        answerCommand(arguments, msg)
+    } else if (primaryCommand == "now"){
+        nowCommand(arguments, msg)
     } else {
         msg.channel.send("Huh~")
     }
@@ -39,11 +43,15 @@ function processCommand(msg) {
 
 function helpCommand(arguments, msg) {
     if (arguments == 'list') {
-        msg.channel.send("As of now, I can help you with these topics: `multiply`, `say` and `prefix`. Still improving!")
+        msg.channel.send("As of now, I can help you with these topics: `multiply`, `say`, `answer`, `now` and `prefix`. Still improving!")
     } else if (arguments == 'multiply') {
         msg.channel.send("I'll gladly multiply the numbers you provide me as long you input more than two values, and only if you promise me to study math: `multiply [value] [value] ...`.")
     } else if (arguments == 'say') {
         msg.channel.send("Do you want me to say something? Just ask *me* to `say [message]`.")
+    } else if (arguments == 'answer') {
+        msg.channel.send("Are you curious about my opinion? Ask *me* to `answer [question]`.")
+    } else if (arguments == 'now') {
+        msg.channel.send("If you want to know how I'm feeling or what I'm doing, just tell *me* `now`")
     } else if (arguments == 'prefix') {
         msg.channel.send("Just call me by my name!")
     } else if (arguments.length < 1) {
@@ -76,6 +84,19 @@ function sayCommand(arguments, msg) {
     msg.channel.send(sayMessage);
 
 }
+
+function answerCommand(arguments, msg) {
+    var options = ["Yes", "No", "Yeah", "Nah", "Maybe", "Maybe not", "I guess", "I guess not", "I don't know, so I can't actually answer.", "I'm not sure.", "Ask someone else.", "Negative", "Negative multiplied by negative.", "Pancakes", "*Impossible*", "I don't know, but you do.", "Do you really want me to answer that?", "Uhhh"]
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+}
+
+function nowCommand(arguments, msg) {
+    var options = ["I'm feeling happy. What about you?", "I'm craving some food.", "It's a beatiful day outside, don't you think so? Flowers are singing, birds are blooming. *Wait, I screwed it up.*", "I'm just having an existential crisis: Does the Lighting McQueen have life insurance or car insurance?", "I'm eating some popcorns.", "I'm planning a way to defeat all those zombies around my Minecraft house.", "I'm *doing homework* but I should be doing homework.", "Have you ever played a videogame world cup and ranked first place? Neither me, but I'm making some coffee.", "I'm having a déjà vu.", "I was reading a book, called 'sʞooq pɐǝɹ oʇ ʍoH', I didn't understand a single word.", "Yesterday, I was back in the mine, got my pickaxe swinging from side to side, hoping to find some diamonds. When I thought I was safe, I overheard some hissing from right behind. Aww man.", "Hate me or love me, I tried playing Fortnite. Apparently I have to download it.", "I saw some memes today.", "I'm bored.", "I'm having a headache.", "I'm online.", "I feel confused, is a *taco* the same thing as a *sope* or an *enchilada*? I mean after all these are *tortillas* with stuff like meat or cheese.", "I'm kinda sad. My ice cream melted before I even tasted it.", "I want to know why roses are red and violets are blue.", "Is life made of 5 protons or why is it so Boron?", "I'm glad to have someone talking to me.", "I heard the convenience store is selling [愛] love. Can you lend me 298 yen?"]
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+}
+
 client.on('message', msg => {
   if (msg.author == client.user) { // Prevent bot from responding to its own messages
       return
