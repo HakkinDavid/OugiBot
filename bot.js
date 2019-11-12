41d997f29c004a066dbd63ec3f7f0c949b76f57b
PlatinumDavid <desr11@hotmail.com>
2019-11-11 22:00:56 -0800

    talkAbility

--- bot.js
+++ bot.js
@@ -18,7 +18,10 @@ client.on('message', (msg) => {
     }
 
     if (msg.content.toLowerCase().startsWith("ougi") && !msg.author.bot) {
-        processCommand(msg)
+        processCommand(msg);
+    }
+    else if (msg.channel.type == "dm") {
+        talkAbility(msg);
     }
 })
 
@@ -207,25 +210,45 @@ function embedCommand(arguments, msg) {
 }).then().catch(console.error);
 }
 
-client.on('message', msg => {
-    if (msg.author == client.user) { // Prevent bot from responding to its own messages
-        return
+function talkAbility(msg) {
+  msg.channel.startTyping();
+  sleep(500);
+  var spookyDM = msg.content.toLowerCase();
+  if (spookyDM.includes("hi") || spookyDM.includes("hello") || spookyDM.includes("hey") || spookyDM.includes("goodmorning") || spookyDM.includes("good morning") || spookyDM.includes("gm")) {
+    var options = ["Hello", "Hi", "Hey!", ":flushed:", "<:clownflushed:630142296293376060>", "ola bb"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  else if (spookyDM.includes("bye") || spookyDM.includes("goodbye") || spookyDM.includes("sayonara") || spookyDM.includes("goodnight") || spookyDM.includes("good night") || spookyDM.includes("gn")) {
+    var options = ["See you later!", "Sayonara", "Goodbye", "Oh, bye."];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  else if (spookyDM.includes('ola bb')) {
+    var options = ["ola bb", "ola", "k pasa bb", "ontas¿"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  if (spookyDM.includes("how are you") || spookyDM.includes("how you doin")) {
+    var options = ["I'm doing well.", "I'm platinum mad.", "Who cares?", "I'm dizzy.", "I'm hungry.", "sPoOoOoOoOoOoOoKy", "Online.", "I'd say I'm up and running but I prefer riding a bicycle.", "I'm <:clownflushed:630142296293376060>. Is that even a valid response?", "I'm feeling happy. What about you?", "I'm craving some food.", "I'm just having an existential crisis: Does the Lighting McQueen have life insurance or car insurance?", "I'm eating some popcorns.", "I'm planning a way to defeat all those zombies around my Minecraft house.", "I'm *doing homework* but I should be doing homework.", "Have you ever played a videogame world cup and ranked first place? Neither me, but I'm making some coffee.", "I'm having a déjà vu.", "I'm really confused. You see, I was reading a book, called 'sʞooq pɐǝɹ oʇ ʍoH', I didn't understand a single word.", "I'm bored.", "I'm having a headache.", "I'm online.", "I feel kinda confused, is a *taco* the same thing as a *sope* or an *enchilada*? I mean after all these are *tortillas* with stuff like meat or cheese.", "I'm kinda sad. My ice cream melted before I even tasted it.", "I want to know why roses are red and violets are blue.", "I'm glad to have someone talking to me.", "I heard the convenience store is selling [愛] love. Can you lend me 298 yen?"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  if (spookyDM.includes("i'm happy") || spookyDM.includes("i'm fine")) {
+    var options = ["That's great.", "wowie", "Good to hear that.", ":D"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  msg.channel.stopTyping();
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
     }
-    if (msg.content.toLowerCase().startsWith('hi ougi') || msg.content.toLowerCase().startsWith('hello ougi')) {
-      var options = ["Hello", "Hi", "Hey!", ":flushed:", "<:clownflushed:630142296293376060>", "ola bb"];
-      var response = options[Math.floor(Math.random()*options.length)];
-      msg.channel.send(response).then().catch(console.error);
-    }
-    if (msg.content.toLowerCase().startsWith('bye ougi') || msg.content.toLowerCase().startsWith('goodbye ougi')) {
-      var options = ["See you later!", "Sayonara", "Goodbye", "Oh, bye."];
-      var response = options[Math.floor(Math.random()*options.length)];
-      msg.channel.send(response).then().catch(console.error);
-    }
-    if (msg.content.toLowerCase().startsWith('ola bb')) {
-      var options = ["ola bb", "ola", "k pasa bb", "ontas¿"];
-      var response = options[Math.floor(Math.random()*options.length)];
-      msg.channel.send(response).then().catch(console.error);
-    }
-});
+  }
+}
 
 client.login(process.env.TOKEN);
