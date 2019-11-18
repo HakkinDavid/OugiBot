9313bd6300d69ad6429eb92dc92ead7b27fc8395
PlatinumDavid <desr11@hotmail.com>
2019-11-17 21:05:49 -0800

    settings and image

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const cheerio = require("cheerio");
+const request = require("request");
 
 client.on('ready', () => {
   var options = ["Ougi: hi", "Ougi: ohayou", "Ougi: baka", "Ougi: hey there!", "Ougi: ola bb", "Ougi joins the battle!", "Creeper. \nOugi: Aw man"];
@@ -20,6 +23,7 @@ client.on('message', (msg) => {
     if (msg.content.toLowerCase().startsWith("ougi") && !msg.author.bot) {
         processCommand(msg);
     }
+
     else if (msg.channel.type == "dm") {
         talkAbility(msg);
     }
@@ -31,8 +35,11 @@ function processCommand(msg) {
     var primaryCommand = splitCommand[1] // The first word directly after Ougi's name is the command
     var arguments = splitCommand.slice(2) // All other words are arguments/parameters/options for the command
 
-    console.log("Command received: " + primaryCommand)
-    console.log("Arguments: " + arguments) // There may not be any arguments
+    var event = new Date();
+    console.log("\n")
+    console.log(event.toLocaleTimeString('en-US'));
+    console.log("Command received: " + primaryCommand);
+    console.log("Arguments: " + arguments);
 
     if (primaryCommand == "help") {
         helpCommand(arguments, msg)
@@ -45,35 +52,30 @@ function processCommand(msg) {
     } else if (primaryCommand == "answer") {
         answerCommand(arguments, msg)
     } else if (primaryCommand == "now"){
-        nowCommand(arguments, msg)
-    } else if (primaryCommand == "meme") {
-        memeCommand(arguments, msg)
+        nowCommand(msg)
+    } else if (primaryCommand == "image") {
+        imageCommand(arguments, msg)
     } else if (primaryCommand == "embed") {
         embedCommand(arguments, msg)
-    } else if (primaryCommand == "flushed") {
-        var options = [":flushed:", "<:clownflushed:630142296293376060>", "<:coolflushed:638924603107967007>", "<:cowboyflushed:638925102238400512>", "<:eggflushed:638924893907451946>"];
-        var response = options[Math.floor(Math.random()*options.length)];
-        msg.channel.send(response).then().catch(console.error);
+    } //else if (primaryCommand == "whisper") {
+        //spookyWhisperCommand(arguments, msg)
+    //} else if (primaryCommand == "spookify") {
+        //spookifyCommand(msg)
+    //}
+    else if (primaryCommand == "flushed") {
+        flushedCommand(arguments, msg)
+    } else if (primaryCommand == "settings") {
+        settingsCommand(arguments, msg)
     } else if (primaryCommand == undefined) {
-        var options = ["I don't get it.", "What do you mean?", "Baka.", "Oh.", "Nani", "Nande"];
-        var response = options[Math.floor(Math.random()*options.length)];
-        msg.channel.send(response).then().catch(console.error);
+        undefinedCommand(arguments, msg)
     } else {
-        var badWord = ["nigga", "faggot", "fuck", "nigger", "baka", "stupid", "dumb", "hentai", "shit", "fucking", "idiot", "silly", "ass", "retard", "whore", "gay"]
-        for (var i = 0; i < badWord.length; i++) {
-            if (msg.content.includes(badWord[i])) {
-              var options = ["no u", "you're a bad word", "then you uhhhhh you're a fortniter", "<:nou:638908430899478540>", "<:reverse:638908430878507018>"];
-              var response = options[Math.floor(Math.random()*options.length)];
-              msg.channel.send(response).then().catch(console.error);
-              break;
-            }
-        }
+        checkBadWords(arguments, msg)
     }
 }
 
 function helpCommand(arguments, msg) {
     if (arguments == 'list') {
-        msg.channel.send("As of now, I can help you with these topics: `multiply`, `add`, `say`, `answer`, `now`, `meme`, `embed` and `prefix`. Still improving!");
+        msg.channel.send("As of now, I can help you with these topics: `multiply`, `add`, `say`, `answer`, `now`, `image`, `embed` and `prefix`. Still improving!");
     } else if (arguments == 'multiply') {
         msg.channel.send("I'll gladly multiply the numbers you provide me as long you input more than two values, and only if you promise me to study math: `multiply [value] [value] ...`.");
     } else if (arguments == 'add') {
@@ -84,13 +86,13 @@ function helpCommand(arguments, msg) {
         msg.channel.send("Are you curious about my opinion? Ask *me* to `answer [question]`.");
     } else if (arguments == 'now') {
         msg.channel.send("If you want to know how I'm feeling or what I'm doing, just tell *me* `now`");
-    } else if (arguments == 'meme'){
-        const attachment = new Discord.Attachment("./images/memehelp.png");
+    } else if (arguments == 'image'){
+        const attachment = new Discord.Attachment("./images/imagehelp.png");
         msg.channel.send(attachment).then().catch(console.error);
     } else if (arguments == 'embed'){
         msg.channel.send('Do you want to make some cool embeds? Try asking *me* ```embed [url] `[title]` [description]```');
     } else if (arguments == 'prefix') {
-        msg.channel.send("Just call me by my name!")
+        msg.channel.send("Just call me by my name!");
     } else if (arguments.length < 1) {
         var options = ["Could you be more specific? Try asking *me* for `help [topic]`. A good start would be `help list`.", "Do you need help? Try asking *me* for `help [topic]`. As an example, use `help list`.", "Is there anything I could help you with? Ask *me* for `help [topic]`. You could try `help list`."];
         var response = options[Math.floor(Math.random()*options.length)];
@@ -152,22 +154,43 @@ function nowCommand(arguments, msg) {
     msg.channel.send(response).then().catch(console.error);
 }
 
-function memeCommand(arguments, msg) {
-    var call = arguments[0];
-    if (call == "list") {
-      var number = arguments[1];
-      var memelist = ["I am inevitable [ID: Inevitable]", "Impossible", "Wallace", "Yes", "Everything", "Did you do it? [ID: Didyoudoit]", "What did it cost? [ID: Whatdiditcost]", "Failure Snap [ID: Failuresnap]", "No", "Too late, 15 years too late. [ID: toolate]"];
-      if (number == "2") {
-        msg.channel.send("Ougi's Meme List (Page 2):\n\`\`\`" + memelist.sort().slice(9).join("\n") + "\`\`\`")
-      }
-      else {
-        msg.channel.send("Ougi's Meme List (Page 1):\n\`\`\`" + memelist.sort().slice(0, 10).join("\n") + "\`\`\`")
-      }
+function imageCommand(arguments, msg) {
+    if (arguments.length < 1) {
+      msg.channel.send("I need at least one keyword to search.")
+      return;
     }
-    else {
-        const attachment = new Discord.Attachment("./images/"+ call + ".png");
+    var search = arguments.join(" ");
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+
+        $ = cheerio.load(responseBody);
+
+        var links = $(".image a.link");
+
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+
+        if (!urls.length) {
+            msg.channel.send("There aren't any results.")
+            return;
+        }
+
+        var imageToSend = urls[Math.floor(Math.random()*urls.length)];
+
+        const attachment = new Discord.Attachment(imageToSend);
+
         msg.channel.send(attachment).then().catch(console.error);
-    }
+        console.log("Sent image: " + imageToSend);
+    });
 }
 
 function embedCommand(arguments, msg) {
@@ -210,6 +233,56 @@ function embedCommand(arguments, msg) {
 }).then().catch(console.error);
 }
 
+//function spookyWhisperCommand(arguments, msg) {
+//  var dmUsers = fs.readFileSync("./dmUsers").toString('utf-8');
+//  var dmAble = dmUsers.split("\n");
+//  var dmID = fs.readFileSync("./dmID");
+//  var spookyIDs = dmID.split("\n");
+//  var finalDestination = arguments[0].replace("@", "")
+//  if (spookyIDs.includes(msg.author.id)) {
+//    if (!dmAble.includes(finalDestination)) {
+//      msg.channel.send("Sorry but your buddy hasn't been spookified. Ask them to tell *me* `spookify`");
+//    }
+//  }
+//  else if (!spookyIDs.includes(msg.author.id)) {
+//    msg.channel.send("In order to spooky-whisper someone else through DMs, you must agree to receive those whisperings aswell. Tell *me* `spookify`");
+//  }
+//}
+
+//function spookifyCommand(msg) {
+//  var pseudoArray = fs.readFileSync('dmID.txt').toString();
+//  var proArray = pseudoArray.split(',');
+//  var callerID = msg.author.id
+//  proArray.push(callerID);
+//  console.log("Discord ID to be added: " + callerID)
+//  fs.writeFile('./dmID.txt', pseudoArray, console.error);
+//}
+
+function flushedCommand(arguments, msg) {
+  var options = [":flushed:", "<:clownflushed:630142296293376060>", "<:coolflushed:638924603107967007>", "<:cowboyflushed:638925102238400512>", "<:eggflushed:638924893907451946>"];
+  var response = options[Math.floor(Math.random()*options.length)];
+  msg.channel.send(response).then().catch(console.error);
+}
+
+function undefinedCommand(arguments, msg) {
+  var options = ["I don't get it.", "What do you mean?", "Baka.", "Oh.", "Nani", "Nande"];
+  var response = options[Math.floor(Math.random()*options.length)];
+  msg.channel.send(response).then().catch(console.error);
+  return
+}
+
+function checkBadWords(arguments, msg) {
+  var badWord = ["nigga", "faggot", "fuck", "nigger", "baka", "stupid", "dumb", "hentai", "shit", "fucking", "idiot", "silly", "ass", "retard", "whore", "gay"]
+  for (var i = 0; i < badWord.length; i++) {
+      if (msg.content.includes(badWord[i])) {
+        var options = ["no u", "you're a bad word", "then you uhhhhh you're a fortniter", "<:nou:638908430899478540>", "<:reverse:638908430878507018>"];
+        var response = options[Math.floor(Math.random()*options.length)];
+        msg.channel.send(response).then().catch(console.error);
+        break;
+      }
+  }
+}
+
 function talkAbility(msg) {
   msg.channel.startTyping();
   sleep(500);
@@ -229,17 +302,67 @@ function talkAbility(msg) {
     var response = options[Math.floor(Math.random()*options.length)];
     msg.channel.send(response).then().catch(console.error);
   }
-  if (spookyDM.includes("how are you") || spookyDM.includes("how you doin")) {
+  else if (spookyDM.includes("how are you") || spookyDM.includes("how you doin")) {
     var options = ["I'm doing well.", "I'm platinum mad.", "Who cares?", "I'm dizzy.", "I'm hungry.", "sPoOoOoOoOoOoOoKy", "Online.", "I'd say I'm up and running but I prefer riding a bicycle.", "I'm <:clownflushed:630142296293376060>. Is that even a valid response?", "I'm feeling happy. What about you?", "I'm craving some food.", "I'm just having an existential crisis: Does the Lighting McQueen have life insurance or car insurance?", "I'm eating some popcorns.", "I'm planning a way to defeat all those zombies around my Minecraft house.", "I'm *doing homework* but I should be doing homework.", "Have you ever played a videogame world cup and ranked first place? Neither me, but I'm making some coffee.", "I'm having a déjà vu.", "I'm really confused. You see, I was reading a book, called 'sʞooq pɐǝɹ oʇ ʍoH', I didn't understand a single word.", "I'm bored.", "I'm having a headache.", "I'm online.", "I feel kinda confused, is a *taco* the same thing as a *sope* or an *enchilada*? I mean after all these are *tortillas* with stuff like meat or cheese.", "I'm kinda sad. My ice cream melted before I even tasted it.", "I want to know why roses are red and violets are blue.", "I'm glad to have someone talking to me.", "I heard the convenience store is selling [愛] love. Can you lend me 298 yen?"];
     var response = options[Math.floor(Math.random()*options.length)];
     msg.channel.send(response).then().catch(console.error);
   }
-  if (spookyDM.includes("i'm happy") || spookyDM.includes("i'm fine")) {
+  else if (spookyDM.includes("i'm happy") || spookyDM.includes("i'm fine")) {
     var options = ["That's great.", "wowie", "Good to hear that.", ":D"];
     var response = options[Math.floor(Math.random()*options.length)];
     msg.channel.send(response).then().catch(console.error);
   }
+  else {
+    undefinedCommand(arguments, msg)
+  }
   msg.channel.stopTyping();
+}
+
+function settingsCommand(arguments, msg) {
+  if (msg.author.id !== "265257341967007758") {
+    var options = ["Ara ara! Only David-senpai is allowed to access my settings", "N-nani? Stop it, my senpai. What are you doing?", "Nani? Nani? Nani? What's going on? Why is my senpai calling me out, using `settings` command and trying to peek at it? :flushed:"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+    return
+  }
+  else if (msg.author.id == "265257341967007758") {
+    var action = arguments[0];
+    if (action == "do") {
+      var fullCommand = msg.content.substr(4); // Remove Ougi's name
+      var splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
+      var primaryCommand = splitCommand[1]; // The first word directly after Ougi's name is the command
+      var arguments = splitCommand.slice(2); // All other words are arguments/parameters/options for the command
+      var newArgs = arguments.join(" ").toString();
+      var newArguments = newArgs.split('"');
+      var name = newArguments[1];
+      var type = newArguments[2];
+      client.user.setActivity(name, { type: type.replace(" ","") })
+      console.log("I'm " + type.replace(" ","") + " " + name)
+      msg.channel.send("Alright, switched! I'm " + type.replace(" ","") + " " + name).then().catch(console.error);
+    }
+    else if (action == "status") {
+      var status = arguments[1];
+      client.user.setStatus(status).then().catch(console.error);
+      console.log("Now I'm " + status.replace("dnd", "in Do Not Disturb mode"));
+      msg.channel.send("Now I'm " + status.replace("dnd", "in Do Not Disturb mode"));
+    }
+    else if (action == "show") {
+      var what = arguments[1];
+      if (what == "emoji") {
+        const emojiList = client.emojis.map((e, x) => (x + ' = ' + e) + ' | ' +e.name).join('\n')
+        fs.writeFile('all.emoji', emojiList, console.error);
+        msg.channel.send("I've wrote a file called all.emoji containing every single emoji I can use");
+      }
+      else if (what == "guilds") {
+        const guildsList = client.guilds.map((e, x) => (x + ' = ' + e)).join('\n')
+        fs.writeFile('all.guilds', guildsList, console.error);
+        msg.channel.send("I've wrote a file called all.guilds containing every single guild I'm in");
+      }
+    }
+    else {
+      undefinedCommand(arguments, msg)
+    }
+  }
 }
 
 function sleep(milliseconds) {
