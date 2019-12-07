efa8032c015aa2ed33179b01e66a9a186fe3fc87
PlatinumDavid <desr11@hotmail.com>
2019-12-07 20:54:50 +0000

    up

--- wakeUp.js
+++ wakeUp.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
 var http = require('http');
+var options = ["hi", "ohayou", "baka", "hey there!", "ola bb", "Ougi joins the battle!", "Creeper. \nAw man"];
+var response = options[Math.floor(Math.random()*options.length)];
 
 http.createServer(function (req, res) {
-  res.write('no');
+  res.write(response);
   res.end();
 }).listen(8080);
