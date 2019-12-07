c5b2144a63c8d24a7033090cc4e4ce2aab4c2d39
PlatinumDavid <desr11@hotmail.com>
2019-12-07 20:36:04 +0000

    no

--- wakeUp.js
+++ wakeUp.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 var http = require('http');
-var html = fs.readFileSync('./ougi.html')
 
 http.createServer(function (req, res) {
-  res.write(html);
+  res.write('no');
   res.end();
 }).listen(8080);
