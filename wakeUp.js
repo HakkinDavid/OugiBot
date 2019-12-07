6f68ef3731c6ea222290adf8c226fb2509de4571
PlatinumDavid <desr11@hotmail.com>
2019-12-07 20:31:53 +0000

    prints html

--- wakeUp.js
+++ wakeUp.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 var http = require('http');
+var html = fs.readFileSync('./ougi.html')
 
 http.createServer(function (req, res) {
-  res.write('./ougi.html');
+  res.write(html);
   res.end();
 }).listen(8080);
