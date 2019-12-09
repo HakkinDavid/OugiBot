21cdcb8dff53eef3ab16f4d478df398ffe397e34
PlatinumDavid <desr11@hotmail.com>
2019-12-08 17:48:08 -0800

    k pasa bb

--- fan.js
+++ fan.js
@@ -381,7 +381,12 @@ function talkAbility(msg) {
     var response = options[Math.floor(Math.random()*options.length)];
     msg.channel.send(response).then().catch(console.error);
   }
-  else if (spookyDM.includes("how are you") || spookyDM.includes("how you doin")) {
+  else if (spookyDM.includes('k pasa bb') || spookyDM.includes('k hay bb') || spookyDM.includes('k ases bb')) {
+    var options = ["nada bb", "aki solita", "tengo amvre", "kiero tu enshilada bb", "a punto de mandarte un paketaxo por fedex"];
+    var response = options[Math.floor(Math.random()*options.length)];
+    msg.channel.send(response).then().catch(console.error);
+  }
+  else if (spookyDM.includes("how are you") || spookyDM.includes("how you doin") || spookyDM.includes("how are u")) {
     var options = ["I'm doing well.", "I'm platinum mad.", "Who cares?", "I'm dizzy.", "I'm hungry.", "sPoOoOoOoOoOoOoKy", "Online.", "I'd say I'm up and running but I prefer riding a bicycle.", "I'm <:clownflushed:630142296293376060>. Is that even a valid response?", "I'm feeling happy. What about you?", "I'm craving some food.", "I'm just having an existential crisis: Does the Lighting McQueen have life insurance or car insurance?", "I'm eating some popcorns.", "I'm planning a way to defeat all those zombies around my Minecraft house.", "I'm *doing homework* but I should be doing homework.", "Have you ever played a videogame world cup and ranked first place? Neither me, but I'm making some coffee.", "I'm having a déjà vu.", "I'm really confused. You see, I was reading a book, called 'sʞooq pɐǝɹ oʇ ʍoH', I didn't understand a single word.", "I'm bored.", "I'm having a headache.", "I'm online.", "I feel kinda confused, is a *taco* the same thing as a *sope* or an *enchilada*? I mean after all these are *tortillas* with stuff like meat or cheese.", "I'm kinda sad. My ice cream melted before I even tasted it.", "I want to know why roses are red and violets are blue.", "I'm glad to have someone talking to me.", "I heard the convenience store is selling [愛] love. Can you lend me 298 yen?"];
     var response = options[Math.floor(Math.random()*options.length)];
     msg.channel.send(response).then().catch(console.error);
