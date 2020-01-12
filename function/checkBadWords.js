e38582ae9238041639e27106a3b1fdca365c141b
PlatinumDavid <desr11@hotmail.com>
2020-01-11 17:15:39 -0800

    insultos insultantes

--- function/checkBadWords.js
+++ function/checkBadWords.js
@@ -1,24 +1,25 @@
 module.exports =
 
 function checkBadWords(arguments, msg) {
-  var badWord = ["nigga", "faggot", "fuck", "nigger", "baka", "stupid", "dumb", "hentai", "shit", "fucking", "idiot", "silly", "ass", "retard", "whore", "gay"]
+  var badWord = ["nigga", "faggot", "fuck", "nigger", "baka", "stupid", "dumb", "hentai", "shit", "fucking", "silly", "ass", "retard", "whore", "gay"]
   for (var i = 0; i < badWord.length; i++) {
       if (msg.content.includes(badWord[i])) {
         var options = ["no u", "you're a bad word", "then you uhhhhh you're a fortniter", "<:nou:638908430899478540>", "<:reverse:638908430878507018>"];
         var response = options[Math.floor(Math.random()*options.length)];
         msg.channel.send(response).then().catch(console.error);
+        var r = true
         break;
-        return;
       }
   }
-  var insultos = ["joto", "puto", "estúpido", "verga", "pendejo", "pendeja", "idiota", "mierda", "tonto", "retrasado", "chupa", "pito", "chinga"]
-  for (var i = 0; i < insultos.length; i++) {
-      if (msg.content.includes(insultos[i])) {
-        var options = ["la tuya por si acaso", "tu existencia es un insulto a la humanidad", "entonces no eres un verdadero fortniter", "<:nou:638908430899478540>", "<:reverse:638908430878507018>"];
-        var response = options[Math.floor(Math.random()*options.length)];
-        msg.channel.send(response).then().catch(console.error);
-        break;
-        return;
+  if (r = true) {
+    var insultos = ["joto", "puto", "estúpido", "verga", "pendejo", "pendeja", "idiota", "mierda", "tonto", "retrasado", "chupa", "pito", "chinga"]
+    for (var i = 0; i < insultos.length; i++) {
+        if (msg.content.includes(insultos[i])) {
+          var options = ["la tuya por si acaso", "tu existencia es un insulto a la humanidad", "entonces no eres un verdadero fortniter", "<:nou:638908430899478540>", "<:reverse:638908430878507018>"];
+          var response = options[Math.floor(Math.random()*options.length)];
+          msg.channel.send(response).then().catch(console.error);
+          break;
       }
+    }
   }
 }
