3b506a534fb50b39e8f4626a7a0589d3a2662017
HakkinDavid <desr11@hotmail.com>
2020-04-29 02:10:35 -0700

    i can make a promise

--- fan.js
+++ fan.js
@@ -7,11 +7,26 @@ global.cheerio = require('cheerio');
 global.request = require('request');
 global.requireAll = require('require-all');
 global.download = require('download-file')
+global.Twit = require('twit')
+global.lastTweet = require('last-tweet')
+
+/* Tsuittamonogatari */
+
+
+global.T = new Twit({
+  consumer_key:         process.env.CKEY,
+  consumer_secret:      process.env.CSECRET,
+  access_token:         process.env.ACCTOKEN,
+  access_token_secret:  process.env.ACCTOKENSECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
 
 /* Kansuumonogatari */
 
 global.ougi = require('require-all')(__dirname + '/function');
 global.backupChannel = "693733162408345670";
+global.wordsChannel = "704954561294761984";
 
 /* Rogumonogatari */
 global.consoleLogging = "647348129078837254";
@@ -36,14 +51,21 @@ client.on('ready', () => {
 
   var options = ["hi", "ohayou", "baka", "hey there!", "ola bb", "Ougi joins the battle!", "Creeper. \nAw man"];
   var response = options[Math.floor(Math.random()*options.length)];
-  var doing = ["Minecraft", "Fortnite", "Destiny 2", "Portal", "Portal 2", "Project 64", "osu!", "Geometry Dash", "Slime Rancher", "Left 4 Dead 2", "Transformice", "Grand Theft Auto V", "Team Fortress 2", "Overwatch", "Undertale", "Dolphin", "Ultimate Custom Night", "Minecraft Windows 10 Edition", "Terraria", "Roblox", "Paladins", "Tom Clancy's Rainbow Six Siege"]
+  var doing = ["Minecraft", "Fortnite", "Destiny 2", "Portal", "Portal 2", "Project 64", "osu!", "Geometry Dash", "Slime Rancher", "Left 4 Dead 2", "Transformice", "Grand Theft Auto V", "Team Fortress 2", "Overwatch", "Undertale", "Dolphin", "Ultimate Custom Night", "Minecraft Windows 10 Edition", "Terraria", "ROBLOX", "Paladins", "Tom Clancy's Rainbow Six Siege"]
   var something = doing[Math.floor(Math.random()*doing.length)];
   client.user.setActivity(something)
-  console.log(response + "\nI'm playing " + something);
+  var ougiStart = response + "\nI'm playing " + something;
+  console.log(ougiStart);
   console.log("\n");
+
+  T.post('statuses/update', { status: ougiStart }, function(err, data, response) {
+    console.log("Tweeted my status into Twitter")
+  })
 });
 
 client.on('message', (msg) => {
+    ougi.asyncTweet().catch(console.error);
+    
     if (msg.author == client.user) { // Prevent bot from responding to its own messages
         return
     }
@@ -60,5 +82,6 @@ client.on('message', (msg) => {
         ougi.talkAbility(msg);
     }
 })
+
 /* Kaishimonogatari */
 client.login(process.env.TOKEN);
