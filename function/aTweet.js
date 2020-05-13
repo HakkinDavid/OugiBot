3925ef92f75578e988cd84620c375a16e7795b89
HakkinDavid <desr11@hotmail.com>
2020-05-13 02:59:56 -0700

    no image :pensive:

--- function/aTweet.js
+++ function/aTweet.js
@@ -5,7 +5,7 @@ function aTweet() {
     var willSay = store * 1;
     console.log("Going to tweet word number: " + willSay);
     var pseudoEnglish = JSON.parse(fs.readFileSync('./spookyWords', 'utf-8', console.error));
-    var pick = ["have", "homies", "is this"];
+    var pick = ["have", "homies"];
     var action = pick[Math.floor(Math.random()*pick.length)];
 
     if (action == "have") {
@@ -18,62 +18,6 @@ function aTweet() {
       var chooseHomie = theHomies[Math.floor(Math.random()*theHomies.length)];
       var contentToSay = "i " + chooseHomie + " " + pseudoEnglish[willSay] + "\n" + "all my homies " + chooseHomie + " " + pseudoEnglish[willSay];
     }
-    else if (action == "is this") {
-      request(settings, function(error, response, responseBody) {
-          var willNotSay = willSay + 1;
-          var search = pseudoEnglish[willNotSay];
-          var settings = {
-              url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-              method: "GET",
-              headers: {
-                  "Accept": "text/html",
-                  "User-Agent": "Chrome"
-              }
-          };
-          if (error) {
-              return;
-          }
-
-          $ = cheerio.load(responseBody);
-
-          var links = $(".image a.link");
-
-          var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-
-          if (!urls.length) {
-              console.log('ERROR: There was no results for my tweet\'s image');
-              return;
-          }
-
-          var imageToSend = urls[1];
-
-          var tFileName = "twitterupload.png"
-
-          download(imageToSend, tFileName);
-
-          var contentToSay = "is this " + pseudoEnglish[willSay] + "?";
-
-          var b64content = fs.readFileSync('./twitterupload.png', { encoding: 'base64' });
-
-          T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-          var mediaIdStr = data.media_id_string
-          var altText = pseudoEnglish[willSay];
-          var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-
-            T.post('media/metadata/create', meta_params, function (err, data, response) {
-              if (!err) {
-                var params = { status: contentToSay, media_ids: [mediaIdStr] }
-
-                T.post('statuses/update', params, function (err, data, response) {
-                  console.log("Tweeted: " + contentToSay)
-                })
-              }
-            })
-          })
-      });
-    }
-    store.channel.send(willSay + 1);
-    return;
     else {
       var contentToSay = "the game"
     }
