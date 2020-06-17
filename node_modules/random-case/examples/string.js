da025f1e9103e8998b37816022b99c3512ed194b
HakkinDavid <desr11@hotmail.com>
2020-06-17 13:52:38 -0700

    idk5???

---
+++ node_modules/random-case/examples/string.js
@@ -0,0 +1,9 @@
+var randomCase = require('../src/randomCase');
+
+var input = 'Test string';
+var randomized = randomCase(input);
+
+console.log(
+  'Input: ' + input + '\n' +
+  'Randomized: ' + randomized
+);
