296421c73bf0db9dae0695aba910164d152838f5
HakkinDavid <desr11@hotmail.com>
2021-02-10 21:39:06 -0800

    error tras error

--- function/speakHelp.js
+++ function/speakHelp.js
@@ -4,7 +4,7 @@ async function (msg) {
   let examplePhrases = ["never gonna give you up", "wait, that's illegal", "what is love? baby don't hurt me", "this is not ASMR"];
   let examplePhrases2 = ["ora ora ora ora", "muda muda muda muda", "dorararararara", "shitsurei, kamimashita", "arararagi-senpai", "hazukashii"];
   let embed = await ougi.helpPreset(msg, "speak");
-  embed.setDescription((await ougi.text(msg, "speakHelpDesc")).replace(/{c}/gi, "`guildlanguage`"}))
+  embed.setDescription((await ougi.text(msg, "speakHelpDesc")).replace(/{c}/gi, "`guildlanguage`"))
   .addField(await ougi.text(msg, "example"), "`ougi speak " + examplePhrases[Math.floor(Math.random()*examplePhrases.length)] + "`")
   .addField(await ougi.text(msg, "speakSpecificLang"), "`ougi speak ::ja " + examplePhrases2[Math.floor(Math.random()*examplePhrases2.length)] + "`")
 
