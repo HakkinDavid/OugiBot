65296115381075d8c032a7ad1a99192b74dbfdc3
HakkinDavid <desr11@hotmail.com>
2021-02-10 22:22:13 -0800

    better guild logging

--- function/logRootCommand.js
+++ function/logRootCommand.js
@@ -1,21 +1,21 @@
 module.exports =
 
 async function (arguments, msg) {
-  var what = arguments[0];
+  let what = arguments[0];
   if (what == "emoji") {
-    var emojiList = client.emojis.cache.map((e, x) => (x + ' = ' + e) + ' | ' + e.name);
-    var logFileName = "allEmoji.txt";
-    fs.writeFileSync(logFileName, emojiList.join('\n'), console.error);
-    const attachment = new Discord.MessageAttachment(logFileName);
+    let emojiList = client.emojis.cache.map((e, x) => (x + ' = ' + e) + ' | ' + e.name);
+    let logFileName = "allEmoji.txt";
+    await fs.writeFile(logFileName, emojiList.join('\n'), console.error);
+    let attachment = new Discord.MessageAttachment(logFileName);
 
     client.channels.cache.get(fileSpace).send("__**Emoji:**__ " + emojiList.length, attachment).catch(console.error);
     msg.channel.send("I've written a file containing every single emoji I can use.").catch(console.error);
   }
   else if (what == "guilds") {
-    var guildsList = client.guilds.cache.map((e, x) => (x + ' = ' + e));
-    var logFileName = "allGuilds.txt";
-    fs.writeFileSync(logFileName, guildsList.join('\n'), console.error);
-    const attachment = new Discord.MessageAttachment(logFileName);
+    let guildsLength = client.guilds.cache.map((g) => g.toString() + " with a total of " + g.memberCount + " users");
+    let logFileName = "allGuilds.txt";
+    await fs.writeFile(logFileName, guildsList.join('\n'), console.error);
+    let attachment = new Discord.MessageAttachment(logFileName);
 
     client.channels.cache.get(fileSpace).send("__**Guilds:**__ " + guildsList.length, attachment).catch(console.error);
     msg.channel.send("I've written a file containing every single guild I'm in.").catch(console.error);
