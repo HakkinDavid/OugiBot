f0d502369a801208e7a82aaced062f1afeb56157
HakkinDavid <desr11@hotmail.com>
2021-10-06 22:35:53 -0700

    Update translateHelp.js

--- function/translateHelp.js
+++ function/translateHelp.js
@@ -3,7 +3,7 @@ module.exports =
 async function (msg) {
   let potentialPhrase = ["Hola, mi nombre es Ougi.", "こんにちわ、僕の名前はOugiだ。", "Yo no hablo español, sólo finjo hacerlo.", "No eres un Fortniter.", "¿Eres tonto o masticas agua?", "您好，我是Ougi。", "Hallo, ich heiße Ougi."];
   let phrase = potentialPhrase[Math.floor(Math.random()*potentialPhrase.length)];
-  translate(phrase, {to: "English", client: 'gtx'}).then(res => {
+  translate(phrase, {to: 'en', client: 'gtx'}).then(res => {
     let embed = new Discord.MessageEmbed()
     .setTitle("Ougi Translate")
     .setColor("#6254E7")
