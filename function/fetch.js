d956b12f1a37e251436fa1d06ff3d6b61bf4f6e8
HakkinDavid <desr11@hotmail.com>
2021-11-01 15:02:29 -0700

    Update fetch.js

--- function/fetch.js
+++ function/fetch.js
@@ -5,9 +5,9 @@ async function (channelID) {
     console.log("Couldn't retrieve database content from " + channelID);
   }
   client.channels.cache.get(channelID).messages.fetch({ limit: 1 }).then(
-    messages => {
+    async (messages) => {
       let lastMessage = messages.first();
-      download(lastMessage.attachments.first().url)
+      await download(lastMessage.attachments.first().url)
       console.log("Retrieved database object at " + lastMessage.attachments.first().url);
     }
   ).catch(console.error);
