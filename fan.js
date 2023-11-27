3c9d6650dacfa1d29b52fb15b58104a4e6a3ad03
HakkinDavid <desr11@hotmail.com>
2023-11-27 15:57:25 -0800

    Update fan.js

--- fan.js
+++ fan.js
@@ -349,17 +349,6 @@ setInterval(
     global.reloadedAmmo = {};
     await ougi.writeFile('./settings.txt', JSON.stringify(settingsOBJ, null, 4), console.error);
     await ougi.backup('./settings.txt', settingsChannel);
-    if (global.logsCount >= 1) {
-        await client.channels.cache.get(consoleLogging).messages.fetch({ limit: global.logsCount }).then(
-          async (messages) => {
-            await messages.forEach((obj) => {
-              obj.delete().catch(O_o=>{});
-            });
-            console.log("Cleaning logs ...");
-          }
-        ).catch(console.error);
-    }
-    global.logsCount = 0;
   },
   300000
 );
