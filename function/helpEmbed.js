79545886ddcbef513d770df7b749e5061ecce6d6
HakkinDavid <desr11@hotmail.com>
2025-01-04 04:31:15 -0800

    Update helpEmbed.js

--- function/helpEmbed.js
+++ function/helpEmbed.js
@@ -39,7 +39,7 @@ async function (msg) {
   embed.setTitle(await ougi.text(msg, "helpTitle"))
   .setDescription(await ougi.text(msg, "helpDesc"))
   .addFields({name: await ougi.text(msg, "helpPrefix"), value: "`ougi`\n" + await ougi.text(msg, "helpPrefixExplanation")})
-  .addFields({name: await ougi.text(msg, "availableCommands"), value: await ougi.text(msg, "availableCommandsList") + ": `" + commandsArray.join("`, `").slice(0,-3) + ". " + await ougi.text(msg, "improving")})
+  .addFields({name: await ougi.text(msg, "availableCommands"), value: await ougi.text(msg, "availableCommandsList") + ": `" + commandsArray.join("`, `") + ". " + await ougi.text(msg, "improving")})
   // pandemic ptsd .addFields({name: "<:COVID19:820957937840816140> " + await ougi.text(msg, "featuredCOVID19modules"), await ougi.text(msg, "availableCommandsListCOVID19") + "\n• `covidnews` - " + await ougi.text(msg, "covidnewsDesc") + "\n• `healthcare` - " + await ougi.text(msg, "healthcareDesc") + "\n• `md` - " + await ougi.text(msg, "medicalDefinitionDesc") + "\n• `covidstats` - " + await ougi.text(msg, "covidStatsDesc")})
   .addFields({name: await ougi.text(msg, "privacyPolicy") + "\n\u200b", value: (await ougi.text(msg, "remindLang")).replace(/{c1}/gi, "`ougi language [lang]`").replace(/{c2}/gi, "`ougi help [command]`")});
 
