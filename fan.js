55fd0d2d0ea09640a4f99c7c3395d6fcf8208591
HakkinDavid <desr11@hotmail.com>
2025-01-04 22:02:22 -0800

    Update fan.js

--- fan.js
+++ fan.js
@@ -135,8 +135,8 @@ console.error = function() {
 /* Chuuimonogatari */
 client.on('ready', async () => {
   findRemoveSync('./', {extensions: ['.txt', '.mp3']});
-  for (fetchChannel in fetchedChannels) {
-    console.log(fetchChannel.file + " fetch returned " + await ougi.fetch(fetchChannel.id, fetchChannel.file));
+  for (ch_prop in fetchedChannels) {
+    console.log(fetchedChannels[ch_prop].file + " fetch returned " + await ougi.fetch(fetchedChannels[ch_prop].id, fetchedChannels[ch_prop].file));
   };
 
   client.channels.cache.get(consoleLogging).send("**INSTANCE ID:** " + instanceID + "\n**DEV:** " + process.env.DEV + "\n**SILENT MODE:** " + !global.TEASEABLE).catch(console.error);
