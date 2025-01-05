22e07ee506a56263983c033fbb947002358b9d41
HakkinDavid <desr11@hotmail.com>
2025-01-04 22:13:37 -0800

    Update fetch.js

--- function/fetch.js
+++ function/fetch.js
@@ -13,22 +13,23 @@ async function (channelID, filename) {
     }, (error) => {
       if (error) {
         console.error(error);
+      } else {
+        console.log("Saved as " + filename + ". Checking...");
+        let checking = setInterval(() => {
+          if (!fs.existsSync(filename)) return;
+          if (ougi.readFile(filename) === undefined) {
+            lastMessage.delete();
+            fs.unlinkSync(filename);
+            console.log(("[EH] Bad file " + filename + " deleted, retrying soon...").yellow);
+          }
+          else {
+            console.log(("[OK] Retrieved " + filename + ".").green);
+            success = true;
+          }
+          clearInterval(checking);
+        }, 500);
       }
     });
-    console.log("Saved as " + filename + ". Checking...");
-    let checking = setInterval(() => {
-      if (!fs.existsSync(filename)) return;
-      if (ougi.readFile(filename) === undefined) {
-        lastMessage.delete();
-        fs.unlinkSync(filename);
-        console.log(("[EH] Bad file " + filename + " deleted, retrying soon...").yellow);
-      }
-      else {
-        console.log(("[OK] Retrieved " + filename + ".").green);
-        success = true;
-      }
-      clearInterval(checking);
-    }, 500);
   }
   return success;
 }
