6dd46edef6551e26690ae31164351546099905e4
HakkinDavid <desr11@hotmail.com>
2025-01-04 20:09:55 -0800

    Revert "foolproof"
    This reverts commit 534e4da30ac3a6b7ca026c38ad5599c22828a54e.

--- function/fetch.js
+++ function/fetch.js
@@ -3,33 +3,15 @@ module.exports =
 async function (channelID) {
   if (client.channels.cache.get(channelID) == undefined) {
     console.log("Couldn't retrieve database content from " + channelID);
-    return;
   }
-  let isValid = false;
-  while (!isValid) {
-    client.channels.cache.get(channelID).messages.fetch({ limit: 1 }).then(
-      async (messages) => {
-          let lastMessage = messages.first();
-          let myURL = lastMessage.attachments.first().url;
-          await download(myURL, {
-            filename: lastMessage.attachments.first().name
-          },
-          (error, filename) => {
-            if (error) { console.error(error); }
-            else {
-              try {
-                JSON.parse(ougi.readFile(filename));
-                isValid = true;
-                console.log("Saved as " + filename);
-              }
-              catch {
-                isValid = false;
-                lastMessage.delete();
-                return;
-              }
-            }});
-            console.log("Retrieved database object at " + myURL);
-          }
-      ).catch(console.error);
-  }
+  client.channels.cache.get(channelID).messages.fetch({ limit: 1 }).then(
+    async (messages) => {
+      let lastMessage = messages.first();
+      let myURL = lastMessage.attachments.first().url;
+      await download(myURL, {
+        filename: lastMessage.attachments.first().name
+      }, (error, filename) => { if (error) { console.error(error); } else { console.log("Saved as " + filename); }});
+      console.log("Retrieved database object at " + myURL);
+    }
+  ).catch(console.error);
 }
