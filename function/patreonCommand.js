191c51dd16eaf1ca3426c779a40bd253c03bd00b
HakkinDavid <desr11@hotmail.com>
2025-08-23 21:14:01 -0700

    Update patreonCommand.js

--- function/patreonCommand.js
+++ function/patreonCommand.js
@@ -5,7 +5,7 @@ module.exports = async function (msg) {
   const patreonEmbed = new EmbedBuilder()
     .setTitle(await ougi.text(msg, "patreonTitle"))
     .setDescription(await ougi.text(msg, "patreonDescription"))
-    .setColor("#000F0")
+    .setColor("#0000F0")
     .setThumbnail("https://github.com/HakkinDavid/OugiBot/blob/master/images/health.png?raw=true")
     .setImage("https://github.com/HakkinDavid/OugiBot/blob/master/images/veryepic.png?raw=true")
     .setFooter({ text: await ougi.text(msg, "patreonFooter") })
