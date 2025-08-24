a554746d14603c9ced41cb75f296639794df5c62
HakkinDavid <desr11@hotmail.com>
2025-08-24 14:01:25 -0700

    Update fan.js

--- fan.js
+++ fan.js
@@ -212,7 +212,7 @@ client.on('messageCreate', async (msg) => {
         if (!settingsOBJ.interactionsCounter.users[msg.author.id]) settingsOBJ.interactionsCounter.users[msg.author.id] = 0;
         if (!settingsOBJ.interactionsCounter.channels[msg.channel.id]) settingsOBJ.interactionsCounter.channels[msg.channel.id] = 0;
         if ((!settingsOBJ.patrons || !settingsOBJ.patrons[msg.author.id]) && (settingsOBJ.interactionsCounter.channels[msg.channel.id] != 0 && settingsOBJ.interactionsCounter.channels[msg.channel.id] % 15 == 0)) {
-            setTimeout(async () => await ougi.patreonCommand(msg, true), 1000);
+            setTimeout(async () => await ougi.patreonCommand(msg, true), 10000);
         }
         settingsOBJ.interactionsCounter.users[msg.author.id] += 1;
         settingsOBJ.interactionsCounter.channels[msg.channel.id] += 1;
