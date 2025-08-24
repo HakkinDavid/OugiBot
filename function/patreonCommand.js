348a3d9415bed9e14d73a79144e6e4a41610be74
HakkinDavid <desr11@hotmail.com>
2025-08-23 21:05:48 -0700

    Update patreonCommand.js

--- function/patreonCommand.js
+++ function/patreonCommand.js
@@ -1,17 +1,14 @@
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = async function (msg, ougi) {
-  const title = await ougi.text(msg, "patreonTitle");
-  const description = await ougi.text(msg, "patreonDescription");
-  const footer = await ougi.text(msg, "patreonFooter");
 
   const patreonEmbed = new EmbedBuilder()
-    .setTitle(title)
-    .setDescription(description)
+    .setTitle(await ougi.text(msg, "patreonTitle"))
+    .setDescription(await ougi.text(msg, "patreonDescription"))
     .setColor("#000F0")
     .setThumbnail("https://github.com/HakkinDavid/OugiBot/blob/master/images/health.png?raw=true")
     .setImage("https://github.com/HakkinDavid/OugiBot/blob/master/images/veryepic.png?raw=true")
-    .setFooter({ text: footer })
+    .setFooter({ text: await ougi.text(msg, "patreonFooter") })
     .setTimestamp();
 
   msg.channel.send({ embeds: [patreonEmbed] });
