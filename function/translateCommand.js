fb2649add1a624a5120b5fbff6fa949cea3a793d
David Emmanuel Santana Romero <desr11@hotmail.com>
2026-08-11 10:21:34 -0700

    petición de knycat (mensajes efímeros)

--- function/translateCommand.js
+++ function/translateCommand.js
@@ -15,7 +15,7 @@ async function (msg) {
       } catch { }
     }
     else {
-      msg.channel.send("Please use a valid syntax for the translation. Refer to the following command if you are clueless.\n> ougi help translate").catch(console.error);
+      msg.channel.send({content: "Please use a valid syntax for the translation. Refer to the following command if you are clueless.\n> ougi help translate", flags: MessageFlags.Ephemeral}).catch(console.error);
       return
     }
   }
@@ -36,7 +36,7 @@ async function (msg) {
   let isLang = ougi.whereIs(ougi.langCodes, niceLang);
   let isCode = ougi.langCodes[toLang];
   if (isLang == undefined && isCode == undefined) {
-    msg.channel.send("Please provide a valid destination language for the translation. Refer to the following command if you are clueless.\n> ougi help translate").catch(console.error);
+    msg.channel.send({content: "Please provide a valid destination language for the translation. Refer to the following command if you are clueless.\n> ougi help translate", flags: MessageFlags.Ephemeral}).catch(console.error);
     return
   }
   if (isCode != undefined && isLang == undefined) {
@@ -69,7 +69,7 @@ async function (msg) {
 
     embed.setFooter({text: "Translated by Ougi", icon: client.user.avatarURL({dynamic: true, size: 4096})})
     .setThumbnail("https://github.com/HakkinDavid/OugiBot/blob/master/images/ougitranslate.png?raw=true");
-    msg.channel.send({embeds: [embed]}).catch(console.error);
+    msg.channel.send({embeds: [embed], flags: MessageFlags.Ephemeral}).catch(console.error);
   }).catch(err => {
       console.error(err);
   });
