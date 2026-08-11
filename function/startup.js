4566421fac9accc9d4a2d245083f43e45e132afc
HakkinDavid <desr11@hotmail.com>
2026-08-11 16:53:07 -0700

    Update startup.js

--- function/startup.js
+++ function/startup.js
@@ -9,16 +9,27 @@ module.exports = function () {
     global.knowledgeBase = dbManager.loadKnowledgeBase() || {};
     global.rafflesOBJ = dbManager.loadRaffles() || {};
 
-    if (!global.settingsOBJ.lang) global.settingsOBJ.lang = {};
-    if (!global.settingsOBJ.prefix) global.settingsOBJ.prefix = {};
+    // Complete 20-key OugiBot settingsOBJ schema initialization
     if (!global.settingsOBJ.banned) global.settingsOBJ.banned = {};
-    if (!global.settingsOBJ.blacklist) global.settingsOBJ.blacklist = {};
-    if (!global.settingsOBJ.ratelimit) global.settingsOBJ.ratelimit = {};
-    if (!global.settingsOBJ.patrons) global.settingsOBJ.patrons = {};
-    if (!global.settingsOBJ.economy) global.settingsOBJ.economy = {};
-    if (!global.settingsOBJ.shortcuts) global.settingsOBJ.shortcuts = {};
-    if (!global.settingsOBJ.guildBump) global.settingsOBJ.guildBump = {};
     if (!global.settingsOBJ.ignored) global.settingsOBJ.ignored = [];
+    if (!global.settingsOBJ.ratelimit) global.settingsOBJ.ratelimit = {};
+    if (!global.settingsOBJ.prefix) global.settingsOBJ.prefix = {};
+    if (!global.settingsOBJ.blacklist) global.settingsOBJ.blacklist = {};
+    if (!global.settingsOBJ.economy) global.settingsOBJ.economy = {};
+    if (!global.settingsOBJ.logging) global.settingsOBJ.logging = {};
+    if (!global.settingsOBJ.lang) global.settingsOBJ.lang = {};
+    if (!global.settingsOBJ.guildNews) global.settingsOBJ.guildNews = {};
+    if (!global.settingsOBJ.subscribers) global.settingsOBJ.subscribers = [];
+    if (!global.settingsOBJ.surveys) global.settingsOBJ.surveys = {};
+    if (!global.settingsOBJ.surveysAvailable) global.settingsOBJ.surveysAvailable = {};
+    if (!global.settingsOBJ.AI) global.settingsOBJ.AI = { description: {} };
+    if (!global.settingsOBJ.guildBump) global.settingsOBJ.guildBump = {};
+    if (!global.settingsOBJ.patreonAdLastSeen) global.settingsOBJ.patreonAdLastSeen = {};
+    if (!global.settingsOBJ.interactionsCounter) global.settingsOBJ.interactionsCounter = {};
+    if (!global.settingsOBJ.patrons) global.settingsOBJ.patrons = {};
+    if (!global.settingsOBJ.shortcuts) global.settingsOBJ.shortcuts = {};
+    if (!global.settingsOBJ.nicknames) global.settingsOBJ.nicknames = {};
+    if (!global.settingsOBJ.guildAdmins) global.settingsOBJ.guildAdmins = {};
   }
   return true;
 };
