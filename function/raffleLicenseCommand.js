6d11d00bd4050a1e89447ef08b26bf507b659342
HakkinDavid <desr11@hotmail.com>
2026-08-12 17:35:59 -0700

    Update raffleLicenseCommand.js

--- function/raffleLicenseCommand.js
+++ function/raffleLicenseCommand.js
@@ -1,9 +1,9 @@
 module.exports = async function (msg) {
     const args = msg.content.split(/\s+/).slice(2);
-    const guildId = args[0] || msg.guildId;
+    const guildId = args[0];
     const durationHours = parseInt(args[1], 10) || 720; // Default 30 days
-    const concurrent = parseInt(args[2], 10) || 5;
-    const participants = parseInt(args[3], 10) || 100;
+    const concurrent = parseInt(args[2], 10) || 1;
+    const participants = parseInt(args[3], 10) || 50;
 
     if (!guildId) {
         msg.channel.send("Usage: `#ougi raffle-license <guild_id> [duration_hours] [concurrent_limit] [participant_limit]`");
